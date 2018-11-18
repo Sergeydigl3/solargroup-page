074522b4a0ac38ae850ae8d7bfa2cbfafd596c66
Serg <Sergeydigl3@gmail.com>
2018-11-18 21:17:42 +0300

    File moved to static directory

--- server.py
+++ server.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-#style ways
-url_for('css', filename='main.css')
-
-#image ways
-url_for('img', filename='bg.jpg')
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
