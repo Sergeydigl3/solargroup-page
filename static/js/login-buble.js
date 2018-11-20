8452e912932e28751963d437a3d4c90131ec9914
Dan <erohin.dn@gmail.com>
2018-11-20 20:07:09 +0300

    logo

--- static/js/login-buble.js
+++ static/js/login-buble.js
@@ -49,3 +49,13 @@ function regForm() {
     }
     else{document.getElementById('enter_window').style.display="block";}
 }
+
+jQuery(function($){
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $("#enter_window"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.hide(); // скрываем его
+        }
+    });
+});
