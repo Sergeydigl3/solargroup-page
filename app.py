a7716615702582a23eafc30c24386b6a6c5c3c51
Serg <Sergeydigl3@gmail.com>
2018-11-20 22:19:53 +0300

    added svg logo

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, make_response
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/beta')
+def beta():
     return render_template('index.html')
+
+@app.route('/')
+def new_html():
+    return render_template('new.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(render_template('404.htm'), 404)
